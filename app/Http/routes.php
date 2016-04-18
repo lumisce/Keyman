6a3fda8b36738896c4fe163467dce2d3553048a0
lumisce <sinehye@gmail.com>
2016-04-18 19:30:15 +0800

    added auth, disabled register

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::group(['middleware' => ['web']], function () {
+
+Route::auth();
+
+Route::get('about', ['middleware' => 'auth', 'uses' => 'PagesController@about']);
+
+// Route::resource('articles', 'ArticlesController');
+
+// Route::get('tags/{tags}', 'TagsController@show');
+
+Route::get('/', ['middleware' => 'guest', 'uses' => 'PagesController@index']);
+
+
+// pagesController = home, about, browse
+// userController = account
+// requestController = index filter=date-asc search=hello page=1, show, create, store, edit, update, destroy
+// customerController = index filter=name-asc search=hello page=1, show, create, store, edit, update, destroy
+// providerController = index filter=name-asc search=hello page=1, show, create, store, edit, update, destroy
+// authController = login, password reset
