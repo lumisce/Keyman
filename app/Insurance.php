7a06aacf88f3262d505b3e654fb30bd5bc48fb31
lumisce <sinehye@gmail.com>
2016-04-21 22:33:13 +0800

    added fillable

--- app/Insurance.php
+++ app/Insurance.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Insurance extends Model
 {
     protected $fillable = [
-        'name',
+        'name', 'insurance_type_id',
     ];
 
     public function provider()
