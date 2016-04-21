aebf02739de14c783d832e4713a3e04c844c8afb
lumisce <sinehye@gmail.com>
2016-04-21 18:12:59 +0800

    added confirm before delete

--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -20,11 +20,19 @@
 	{!! Form::close() !!}
 
 	@if (Auth::user()->isAdmin())
-		{!! Form::open(['method' => 'DELETE', 'action' => ['CustomersController@destroy', $customer->id]]) !!}
+		{!! Form::open(['method' => 'DELETE', 'action' => ['CustomersController@destroy', $customer->id], 'id' => 'deleteForm']) !!}
 			<fieldset class="form-group"> 
 				{!! Form::submit('Delete Customer', ['class' => 'btn btn-danger']) !!}
 			</fieldset>
 		{!! Form::close() !!}
 	@endif
 
+@stop
+
+@section('footer')
+	<script>
+	    $("#deleteForm").on("submit", function(){
+	        return confirm("Do you want to delete this item?");
+	    });
+	</script>
 @stop
--- resources/views/providers/edit.blade.php
+++ resources/views/providers/edit.blade.php
@@ -20,11 +20,19 @@
 	{!! Form::close() !!}
 
 	@if (Auth::user()->isAdmin())
-		{!! Form::open(['method' => 'DELETE', 'action' => ['ProvidersController@destroy', $provider->id]]) !!}
+		{!! Form::open(['method' => 'DELETE', 'action' => ['ProvidersController@destroy', $provider->id], 'id' => 'deleteForm']) !!}
 			<fieldset class="form-group"> 
 				{!! Form::submit('Delete Provider', ['class' => 'btn btn-danger']) !!}
 			</fieldset>
 		{!! Form::close() !!}
 	@endif
 
+@stop
+
+@section('footer')
+	<script>
+	    $("#deleteForm").on("submit", function(){
+	        return confirm("Do you want to delete this item?");
+	    });
+	</script>
 @stop
