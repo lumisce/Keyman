fab061964929bd51e1773103fb159bff6505e774
lumisce <sinehye@gmail.com>
2016-04-28 21:14:22 +0800

    refactored errors._list

--- resources/views/insurances/create.blade.php
+++ resources/views/insurances/create.blade.php
@@ -5,15 +5,7 @@
 
 	<hr>
 
-	@if (count($errors) > 0)
-	    <div class="alert alert-danger">
-	        <ul>
-	            @foreach ($errors->all() as $error)
-	                <li>{{ $error }}</li>
-	            @endforeach
-	        </ul>
-	    </div>
-	@endif
+	@include('errors._list')
 
 	{!! Form::model($insurance = new \App\Insurance, ['url' => 'providers/'. Request::segment(2) .'/plans']) !!}
 		@include('insurances._form', ['submitButtonText' => 'Add Insurance'])
