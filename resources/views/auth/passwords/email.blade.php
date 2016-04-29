51c6f635f972001010bd0d674b3d154fd800ece6
Irene Therese <theabermejo@gmail.com>
2016-04-29 12:31:48 +0800

    more fixes on the erased files

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,12 +1,21 @@
 @extends('layouts.app')
+<style>
+@media (min-width: 992px) {
 
+    .margin{
+        margin-left: 10%;
+        margin-right: 10%;
+    }
+}
+
+</style>
 <!-- Main Content -->
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+                <div class="panel-heading"></div>
                 <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
@@ -16,11 +25,13 @@
 
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
                         {!! csrf_field() !!}
-
+                        <br />
+                        <div class="center">Send a password reset link to your e-mail.</div>
+                        <br />
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
+                            <label class="blue narrow col-md-3 control-label">E-MAIL: </label>
 
-                            <div class="col-md-6">
+                            <div class="center col-md-6">
                                 <input type="email" class="form-control" name="email" value="{{ old('email') }}">
 
                                 @if ($errors->has('email'))
@@ -31,10 +42,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                        <div class="center form-group">
+                            <div class="col-md-6 col-md-offset-3">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-envelope"></i>Send Password Reset Link
+                                    <i class="fa fa-btn fa-envelope"></i>Send
                                 </button>
                             </div>
                         </div>
