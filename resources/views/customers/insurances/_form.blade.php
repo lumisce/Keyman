246d127da01c89aeceeab2a449d697d289390c40
nikkidomingo <nikkiii.domingo@gmail.com>
2016-05-09 13:05:55 +0800

    changes in layout

--- resources/views/customers/insurances/_form.blade.php
+++ resources/views/customers/insurances/_form.blade.php
@@ -1,14 +1,17 @@
-{{-- 		<fieldset class="form-group">
+
+<link rel="stylesheet" href="/assets/css/add-edit.css">
+{{-- 	<fieldset class="form-group">
 			{!! Form::label('type', 'Type:') !!}
 			{!! Form::select('type', $types, isset($selected) ? $selected : 0, ['class' => 'form-control', 'id' => 'type-select']) !!}
 		</fieldset>
  --}}
 		<fieldset class="form-group">
-			{!! Form::label('name', 'Plan:') !!}
-			{!! Form::select('name', $plans, isset($selPlan) ? $selPlan : 0, ['class' => 'form-control', 'id' => 'plan-select']) !!}
+			{!! Form::label('name', 'Plan:', ['class' => 'label-requests col-md-3']) !!}
+			{!! Form::select('name', $plans, isset($selPlan) ? $selPlan : 0, ['class' => 'form-control col-md-9', 'id' => 'plan-select']) !!}
 		</fieldset>
 
-		<fieldset class="form-group"> 
+		<fieldset class="form-group">
+			{!! Form::label('', '', ['class' => 'label-requests col-sm-6 col-sm-3 col-sm-offset-6']) !!} 
 			{!! Form::submit($submitButtonText, ['class' => 'btn btn-primary']) !!}
 		</fieldset>
 
