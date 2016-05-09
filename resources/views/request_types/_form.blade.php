d9a17b871d29178d9683c57079fde54697890206
nikkidomingo <nikkiii.domingo@gmail.com>
2016-05-09 13:07:33 +0800

    changes in layout

--- resources/views/request_types/_form.blade.php
+++ resources/views/request_types/_form.blade.php
@@ -1,16 +1,24 @@
+	<link rel="stylesheet" href="/assets/css/add-edit.css">
+	<style type="text/css">
+		.form-control {
+		    text-align: left;
+		    font-family: 'Nav';
+		    width: 50%;
+		}
+	</style>
 		<fieldset class="form-group">
-			
-			{!! Form::label('name', 'Name:') !!}
-			{!! Form::text('name', null, ['class' => 'form-control']) !!}
+			{!! Form::label('name', 'Name:', ['class' => 'label-requests col-xs-6']) !!}
+			{!! Form::text('name', null, ['class' => 'col-sm-6 form-control']) !!}
 		</fieldset>
 
 		<fieldset class="form-group">
 			
-			{!! Form::label('ideal_turnaround', 'Ideal Turnaround (days):') !!}
-			{!! Form::input('number', 'ideal_turnaround', $type->ideal_turnaround ? $type->ideal_turnaround : 1, ['class' => 'form-control']) !!}
+			{!! Form::label('ideal_turnaround', 'Ideal Turnaround (days):', ['class' => 'label-requests col-xs-6']) !!}
+			{!! Form::input('number', 'ideal_turnaround', $type->ideal_turnaround ? $type->ideal_turnaround : 1, ['class' => 'col-sm-6 form-control']) !!}
 		</fieldset>
 
 		<fieldset class="form-group"> 
-			
+
+			{!! Form::label('', '', ['class' => 'label-requests col-sm-6 col-sm-3 col-sm-offset-6']) !!}
 			{!! Form::submit($submitButtonText, ['class' => 'btn btn-primary']) !!}
 		</fieldset>
