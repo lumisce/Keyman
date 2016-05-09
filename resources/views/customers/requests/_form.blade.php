a333298bed0f639046d4b7d1e1305850bb881756
nikkidomingo <nikkiii.domingo@gmail.com>
2016-05-09 13:05:12 +0800

    changes in layout

--- resources/views/customers/requests/_form.blade.php
+++ resources/views/customers/requests/_form.blade.php
@@ -1,14 +1,17 @@
+	
+<link rel="stylesheet" href="/assets/css/add-edit.css">
 		<fieldset class="form-group">
-			{!! Form::label('insurance_id', 'Insurance:') !!}
-			{!! Form::select('insurance_id', $plans, isset($selected) ? $selected : 0, ['class' => 'form-control', 'id' => 'plan-select']) !!}
+			{!! Form::label('insurance_id', 'Insurance:', ['class' => 'label-requests col-md-3']) !!}
+			{!! Form::select('insurance_id', $plans, isset($selected) ? $selected : 0, ['class' => 'form-control col-md-9', 'id' => 'plan-select']) !!}
 		</fieldset>
 
 		<fieldset class="form-group">
-			{!! Form::label('request_type_id', 'Request Type:') !!}
-			{!! Form::select('request_type_id', $types, isset($selType) ? $selType : 0, ['class' => 'form-control', 'id' => 'type-select']) !!}
+			{!! Form::label('request_type_id', 'Request Type:', ['class' => 'label-requests col-md-3']) !!}
+			{!! Form::select('request_type_id', $types, isset($selType) ? $selType : 0, ['class' => 'form-control col-md-9', 'id' => 'type-select']) !!}
 		</fieldset>
 
-		<fieldset class="form-group"> 
+		<fieldset class="form-group">
+			{!! Form::label('', '', ['class' => 'label-requests col-sm-6 col-sm-3 col-sm-offset-6']) !!}
 			{!! Form::submit($submitButtonText, ['class' => 'btn btn-primary']) !!}
 		</fieldset>
 		
--- resources/views/customers/requests/create.blade.php
+++ resources/views/customers/requests/create.blade.php
@@ -1,15 +1,22 @@
 @extends('layouts.app')
 
+<link rel="stylesheet" href="/assets/css/add-edit.css">
+
 @section('content')
-	<h1>Add a New Request to {{ $customer->fullName }}</h1>
-
 	<hr>
-
 	@include('errors._list')
-
-	{!! Form::model($krequest = new \App\KeymanRequest, ['action' => ['RequestsController@store', $customer->id]]) !!}
-		@include('customers.requests._form', ['submitButtonText' => 'Add Request'])
-	{!! Form::close() !!}
+	<div class="col-sm-6 col-sm-3 col-sm-offset-2">
+		<div class="title-box">
+			<center>Add a New Request to {{ $customer->fullName }}</center>
+		</div>
+	</div>
+	<div class="col-md-8 col-md-offset-2">
+		<div class= "panel panel-default">
+			{!! Form::model($krequest = new \App\KeymanRequest, ['action' => ['RequestsController@store', $customer->id]]) !!}
+				@include('customers.requests._form', ['submitButtonText' => 'Add'])
+			{!! Form::close() !!}
+		</div>
+	</div>
 
 @stop
 
