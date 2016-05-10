2e54a414bb366fdfa996eb08506dcd5d8264d130
lumisce <sinehye@gmail.com>
2016-05-10 13:48:11 +0800

    fixed missing ':' in register form

--- resources/views/auth/_form.blade.php
+++ resources/views/auth/_form.blade.php
@@ -1,28 +1,28 @@
 <link rel="stylesheet" href="/assets/css/add-edit.css">
 <fieldset class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-    <label class="label-requests col-xs-3">Name</label>
+    <label class="label-requests col-xs-3">Name:</label>
     {!! Form::text('name', null, ['class' => 'form-control col-sm-9']) !!}
     
 </fieldset>
 
 <fieldset class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-    <label class="label-requests col-xs-3">Email</label>
+    <label class="label-requests col-xs-3">Email:</label>
     {!! Form::email('email', null, ['class' => 'form-control col-sm-9']) !!}
 </fieldset>
 
 <fieldset class="form-group{{ $errors->has('phone_num') ? ' has-error' : '' }}">
-    <label class="label-requests col-xs-3">Phone Number</label>
+    <label class="label-requests col-xs-3">Phone Number:</label>
     {!! Form::text('phone_num', null, ['class' => 'form-control col-sm-9']) !!}
 
 </fieldset>
 
 <fieldset class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-    <label class="label-requests col-xs-3">Password</label>
+    <label class="label-requests col-xs-3">Password:</label>
     {!! Form::password('password', ['class' => 'form-control col-sm-9']) !!}
 </fieldset>
 
 <fieldset class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-    <label class="label-requests col-xs-3">Confirm Password</label>
+    <label class="label-requests col-xs-3">Confirm Password:</label>
     {!! Form::password('password_confirmation', ['class' => 'form-control col-sm-9']) !!}
 </fieldset>
 
