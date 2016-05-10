6e4e92d879ded6c1c3242631bb62338793e9de3b
lumisce <sinehye@gmail.com>
2016-05-10 20:10:16 +0800

    added docs to CustomerInsuranceController, shortened store method

--- app/Http/Controllers/CustomerInsurancesController.php
+++ app/Http/Controllers/CustomerInsurancesController.php
@@ -17,15 +17,26 @@ class CustomerInsurancesController extends Controller
         $this->middleware('admin', ['only' => ['destroy']]);
     }
 
+    /**
+     * Shows Add Insurance To Customer form
+     * @param  Request $request
+     * @return view
+     */
     public function create(Request $request)
     {
         $customer = Customer::findOrFail($request->segment(2));
         $plans = \DB::table('insurances')->join('providers', 'providers.id', '=', 'insurances.provider_id')->select(\DB::raw("CONCAT(insurances.name, ' -- ', providers.name) AS full_name, insurances.id"))->pluck('full_name', 'id');
         $plans = collect($plans);
         $plans->prepend(null, 0);
+
         return view('customers.insurances.create', compact('plans', 'customer'));
     }
 
+    /**
+     * Adds Insurance to Customer after validation
+     * @param  Request $request
+     * @return redirect to Customer Details page
+     */
     public function store(Request $request)
     {
         $customer = Customer::findOrFail($request->segment(2));
@@ -37,14 +48,20 @@ class CustomerInsurancesController extends Controller
         if (!$customer->insurances->contains($request['insurance_id'])) {
             $customer->insurances()->attach($request['insurance_id'], ['valid_until' => $valid]);
             flash()->success(Insurance::findOrFail($request['insurance_id'])->name . ' has been added!');
-            return redirect()->route('customers.show', [$customer]);
+        } else {
+            flash()->error(Insurance::findOrFail($request['insurance_id'])->name . ' already exists!');
         }
-        flash()->error(Insurance::findOrFail($request['insurance_id'])->name . ' already exists!');
         return redirect()->route('customers.show', [$customer]);
 
     }
 
-    // admin only
+    /**
+     * Removes Insurance from Customer
+     * Admin only
+     * @param  Customer  $customer
+     * @param  Insurance $insurance
+     * @return redirect to Customer Details page
+     */
     public function destroy(Customer $customer, Insurance $insurance)
     {
         $customer->insurances()->detach($insurance->id);
@@ -52,10 +69,15 @@ class CustomerInsurancesController extends Controller
         $deletedRows = $customer->requests()->whereIn('insurance_id', $ids)->delete();
         $customer->total_requests = $customer->requests()->count();
         $customer->save();
+
         flash()->success($insurance->name . ' has been removed!');
         return redirect()->route('customers.show', [$customer]);
     }
 
+    /**
+     * Validation Rules
+     * @return array
+     */
     private function getRules()
     {
         return [
