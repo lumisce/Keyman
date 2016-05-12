5a99fc485f074fb91fa6aa69e6aba82da1f3bbca
lumisce <sinehye@gmail.com>
2016-05-12 22:23:45 +0800

    seeded more data PHP ARTISAN MIGRATE:REFRESH --SEED

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,22 +19,148 @@ class DatabaseSeeder extends Seeder
                 'phone_num' => '09112223333',
                 'password' => bcrypt('password'),
                 'is_admin' => true,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ]
         );
         DB::table('insurance_types')->insert([
-            ['name' => 'Life'],
-            ['name' => 'Health'],
-            ['name' => 'Motor'],
-            ['name' => 'Travel'],
+            [
+                'name' => 'Life',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'Health',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'Motor',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'Travel',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            
+
         ]);
         DB::table('request_types')->insert([
             [
                 'name' => 'Addition',
                 'ideal_turnaround' => 3,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
             [
                 'name' => 'Cancellation',
                 'ideal_turnaround' => 5,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+        ]);
+        DB::table('providers')->insert([
+            [
+                'name' => 'Provider A',
+                'location' => 'Makati City',
+                'email' => 'provider_a@email.com',
+                'phone_num' => '09345283034',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'Provider B',
+                'location' => 'Marikina City',
+                'email' => 'provider_b@email.com',
+                'phone_num' => '09065205834',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+        ]);
+        DB::table('insurances')->insert([
+            [
+                'name' => 'A Health 1000',
+                'insurance_type_id' => 2,
+                'provider_id' => 1,
+                'payment' => 1000.00,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'A Travel 500',
+                'insurance_type_id' => 4,
+                'provider_id' => 1,
+                'payment' => 500.00,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'B Health 800',
+                'insurance_type_id' => 2,
+                'provider_id' => 2,
+                'payment' => 800.00,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'B Life 500',
+                'insurance_type_id' => 1,
+                'provider_id' => 2,
+                'payment' => 500.00,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+        ]);
+        DB::table('customers')->insert([
+            [
+                'first_name' => 'Candy',
+                'last_name' => 'Park',
+                'middle_name' => '',
+                'email' => 'candy@email.com',
+                'phone_num' => '09345283034',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'first_name' => 'Irene',
+                'last_name' => 'Bermejo',
+                'middle_name' => 'Yuson',
+                'email' => 'irene@email.com',
+                'phone_num' => '09065205834',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+        ]);
+        DB::table('customer_insurances')->insert([
+            [
+                'insurance_id' => 1,
+                'customer_id' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'valid_until' => Carbon::now()->addYear(1)->format('Y-m-d H:i:s'),
+            ],
+            [
+                'insurance_id' => 3,
+                'customer_id' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'valid_until' => Carbon::now()->addYear(1)->format('Y-m-d H:i:s'),
+            ],
+            [
+                'insurance_id' => 2,
+                'customer_id' => 2,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'valid_until' => Carbon::now()->addYear(1)->format('Y-m-d H:i:s'),
+            ],
+            [
+                'insurance_id' => 3,
+                'customer_id' => 2,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'valid_until' => Carbon::now()->addYear(1)->format('Y-m-d H:i:s'),
             ],
         ]);
     }
